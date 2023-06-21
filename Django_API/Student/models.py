740c2a66a82e6b0bd25a5adcf5caa9baee05c541
WiemFd <wiemfeddaoui99@gmail.com>
2023-06-21 13:39:09 +0100

    update commit

--- Django_API/Student/models.py
+++ Django_API/Student/models.py
@@ -7,13 +7,19 @@ class Students(models.Model):
     FirstName = models.CharField(max_length=50)
     LastName = models.CharField(max_length=50)
     DateOfBirth = models.DateField()
+    def __str__(self):
+        return f"{self.FirstName} {self.LastName}"
 
 class Subjects(models.Model):
     SubjectID = models.AutoField(primary_key=True)
     Name = models.CharField(max_length=50)
     Coefficient = models.IntegerField()
+    def __str__(self):
+        return self.Name
 
 class Grades(models.Model):
-    Student = models.ForeignKey (Students, on_delete=models.CASCADE)
+    Student = models.ForeignKey(Students, on_delete=models.CASCADE)
     Subject = models.ForeignKey(Subjects, on_delete=models.CASCADE)
     Value = models.DecimalField(max_digits=4, decimal_places=2)
+    def __str__(self):
+        return f"{self.Student.FirstName} {self.Student.LastName} - {self.Subject.Name}"
